cb69cca4bc5a2cdc1de4944a9308a33306a6b5b4
Ahsham Alam <ahshamalam24@gmail.com>
2025-01-17 04:16:35 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Insta from "./components/Insta";
 export default function Home() {
   return (
     <div>
-      <Navbar>
+      <Navbar/>
       <HeroSection/>
       <Heroimg/>
       <TopPicks/>
